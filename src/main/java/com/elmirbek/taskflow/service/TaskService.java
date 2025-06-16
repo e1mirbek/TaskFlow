9b6313f507f8ecf152814148578378f03700076b
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-16 18:10:15 +0600

    Добавлен TaskService с методами для получения, сохранения и удаления задач

--- src/main/java/com/elmirbek/taskflow/service/TaskService.java
+++ src/main/java/com/elmirbek/taskflow/service/TaskService.java
@@ -17,4 +17,18 @@ public class TaskService {
   public   List<Task> getAllTasks () {
         return taskRepository.findAll();
     }
+
+    public Task getTaskById (Long id) {
+    return   taskRepository.findById(id).orElse(null);
+    }
+
+    public void saveTask (Task task) {
+      taskRepository.save(task);
+    }
+
+    public void deleteTask (Long id) {
+      taskRepository.deleteById(id);
+    }
+
+
 }
