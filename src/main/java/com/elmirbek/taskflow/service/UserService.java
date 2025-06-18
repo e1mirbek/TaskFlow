0eca049d7d233e4cd5de8b25b64cf122be743a83
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-18 13:05:13 +0600

    Добавлен UserService с реализацией UserDetailsService

--- src/main/java/com/elmirbek/taskflow/service/UserService.java
+++ src/main/java/com/elmirbek/taskflow/service/UserService.java
@@ -4,17 +4,25 @@ package com.elmirbek.taskflow.service;
 import com.elmirbek.taskflow.entity.User;
 import com.elmirbek.taskflow.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
 
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
     }
 }
